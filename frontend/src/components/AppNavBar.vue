53b544762014ebba4d3d39a5a47e807e858dba01
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-02-08 14:20:23 -0300

    Modifica component de navbar

--- frontend/src/components/AppNavBar.vue
+++ frontend/src/components/AppNavBar.vue
@@ -2,17 +2,19 @@
   <v-app-bar>
     <v-app-bar-title>{{ title }}</v-app-bar-title>
     <template #append>
-      <v-btn icon="mdi-heart" :to="{ name: 'base-home' }"></v-btn>
-      <v-btn icon="mdi-magnify"></v-btn>
+      <v-btn v-if="loggedUser" icon="mdi-cart" :to="{ name: 'base-home' }">
+      </v-btn>
       <v-btn
         :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
         @click.stop="themeClick"></v-btn>
-
       <v-btn icon="mdi-dots-vertical">
         <v-icon icon="mdi-dots-vertical" />
         <v-menu activator="parent">
           <v-list>
-            <v-list-item :to="{ name: 'accounts-logout' }"> Sair </v-list-item>
+            <v-list-item v-if="loggedUser" :to="{ name: 'new-product' }"> Adicionar Produto </v-list-item>
+            <v-list-item v-if="loggedUser" :to="{ name: 'accounts-logout' }"> Deslogar </v-list-item>
+            <v-list-item v-if="canAddProduct()" :to="{ name: 'accounts-login' }"> Login </v-list-item>
+            <v-list-item :to="{ name: 'base-home' }"> Página inicial </v-list-item>
           </v-list>
         </v-menu>
       </v-btn>
@@ -21,12 +23,17 @@
 </template>
 
 <script>
+
+import { mapState } from "pinia"
+import { useAccountsStore } from "@/stores/accountsStore"
+import { useProductStore } from "@/stores/productsStore"
+
 export default {
   props: {
     title: {
       type: String,
       required: false,
-      default: "MyApp",
+      default: "Dolceria",
     },
     theme: {
       type: String,
@@ -42,6 +49,15 @@ export default {
     themeClick() {
       this.$emit("themeClick")
     },
+    canAddProduct() {
+      if(this.loggedUser){
+        return this.loggedUser.permissions.ADMIN
+      }
+    }
+  },
+    computed: {
+    ...mapState(useAccountsStore, ["loggedUser"]),
+    ...mapState(useProductStore, ["productStore"])
   },
 }
 </script>
