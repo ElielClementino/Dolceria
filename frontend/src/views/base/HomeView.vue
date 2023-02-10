1f5f7979e8de3f720eab9f10c28db18ece04f2bc
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-02-10 20:36:52 -0300

    add new homeview style

--- frontend/src/views/base/HomeView.vue
+++ frontend/src/views/base/HomeView.vue
@@ -1,19 +1,24 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center text-center fill-height">
-      <v-img contain height="240" src="@/assets/logo.svg" />
-      <blockquote class="blockquote text-h5">
-        &#8220;Primeiro, resolva o problema. Depois, escreva o código.&#8221;
-        <footer>
+      <blockquote class="blockquote text-h4">
+        Bem Vindo a Dolceria do Eliel.
+        <footer >
           <small>
-            <em>&mdash; John Johnson &mdash;</em>
+            <em>Aqui você encontrará diversos tipos de doces </em>
           </small>
         </footer>
       </blockquote>
+      <v-img contain height="310" src="@/assets/Dc.png" />
 
-      <div class="text-body-1 mb-n1 pt-4">Bem-vindo ao template</div>
-      <h1 class="text-h2 font-weight-bold">Djàvue</h1>
-      <code>Vue3 + Vuetify + Vite</code>
+      <div 
+      class="text-h6 mb-4 pt-4"
+      >
+        Somos uma loja que procura entregar o melhor para o nosso cliente, 
+        portanto usamos os produtos da melhor qualidade possível, 
+        e fazemos o máximo para podermos entregar produtos de ótima qualidade para os nossos clientes.
+      </div>
+      <h1 class="text-h2 font-weight-bold">Dolceria</h1>
 
       <div class="py-6" />
 
@@ -21,14 +26,24 @@
         <v-col cols="auto">
           <v-btn
             color="primary"
-            :to="{ name: 'base-getstarted' }"
+            :to="{ name: 'products-list' }"
             min-width="228"
-            rel="noopener noreferrer"
             size="x-large"
             variant="flat"
             class="my-4">
-            <v-icon icon="mdi-speedometer" size="large" start />
-            Get Started
+            <v-icon icon="mdi-cupcake" size="40" start />
+            Veja nossos produtos
+          </v-btn>
+          <v-btn
+            color="primary"
+            min-width="228"
+            rel="noopener noreferrer"
+            v-if="!loggedUser"
+            size="x-large"
+            variant="flat"
+            :to="{ name: 'accounts-register' }">
+            <v-icon icon="mdi-account-arrow-right-outline" size="large" start />
+            Cadastre-se
           </v-btn>
           <v-btn
             v-if="!loggedUser"
@@ -52,18 +67,6 @@
             :to="{ name: 'accounts-logout' }">
             <v-icon icon="mdi-account-arrow-right-outline" size="large" start />
             Logout
-          </v-btn>
-          <v-btn
-            v-if="loggedUser"
-            color="primary"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            variant="flat"
-            :to="{ name: 'tasks-list' }"
-            class="my-4">
-            <v-icon icon="mdi-folder-star-multiple" size="large" start />
-            tarefas
           </v-btn>
         </v-col>
       </v-row>
