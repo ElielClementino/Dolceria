178653e5629600ccaf8c4e893be898529644868a
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-02-10 22:02:27 -0300

    add thanks view

---
+++ frontend/src/views/base/ThanksView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <blockquote class="blockquote text-h4">
+        Muito obrigado pela preferência
+      <div 
+      class="text-h6 mb-4 pt-4"
+      >
+        Espero que nossos produtos sejam do seu agrado e que possamos nos encontrar mais vezes nessa página <v-icon>mdi-cupcake</v-icon>
+      </div>
+      </blockquote>
+      <v-img contain height="310" src="@/assets/Dc.png" />
+    <footer >
+        <small>
+        <em>Até a próxima </em>
+        </small>
+    </footer>
+    </v-responsive>
+  </v-container>
+</template>
+
